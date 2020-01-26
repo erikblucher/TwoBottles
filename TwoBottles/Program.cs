8a75ed438b485aa52a24c4c5c0e3be5d9e480f0b
Erik Blücher <erik.blucher@gmail.com>
2020-01-26 12:44:31 +0100

    First commit

---
+++ TwoBottles/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoBottles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pour from bottle to bottle to measure specific amount of water
+            // Bottles are 3 and 5 litres, 
+            // measure shortest way to measure x litre(s).
+            // Two possible directions for pouring -->
+            // 1) To small bottle
+            // 2) To big bottle
+            // Stop when any bottle contains the sought amount of water
+
+            int smallBottle = 3;
+            int bigBottle = 5;
+            Console.WriteLine("Hur många liter (0-5) vill du mäta upp?");
+            int soughtLitres = 0;
+            try
+            {
+                soughtLitres = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(160);
+            }
+            while (soughtLitres < 0 || soughtLitres > 5)
+            {
+                if (soughtLitres < 0)
+                {
+                    Console.WriteLine("Du kan ej ange ett negativt tal.");
+                }
+                if (soughtLitres > 5)
+                {
+                    Console.WriteLine("Du kan ej ange ett tal som är större än 5.");
+                }
+                try
+                {
+                    soughtLitres = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Environment.Exit(160);
+                }
+                
+            }
+
+            // Pour from both directions
+            // Return smallest value
+            var fromSmallToBigBottleMoves = Pour(smallBottle, bigBottle, soughtLitres);
+            //var leftMoves = PourLeft(smallBottle, bigBottle, soughtLitres);
+            var fromBigToSmallBottleMoves = Pour(bigBottle, smallBottle, soughtLitres);
+
+            Console.WriteLine("Kortaste antal steg: " + ShortestMoves(fromSmallToBigBottleMoves, fromBigToSmallBottleMoves));
+        }
+
+        public static int Pour(int startBottle, int destinationBottle, int soughtLitres)
+        {
+            var moves = 0;
+            var waterInBottle1 = 0;
+            var waterInBottle2 = 0;
+            Console.WriteLine();
+            Console.WriteLine("Häll från " + startBottle + "-litersflaskan till " + destinationBottle + "-litersflaskan:");
+
+            // Loop as long as we have no solution
+            while (waterInBottle1 != soughtLitres && waterInBottle2 != soughtLitres)
+            {
+                if (waterInBottle1 == 0)
+                {
+                    waterInBottle1 = startBottle;
+                    moves++;
+                    Console.WriteLine("Steg " + moves + " (" + waterInBottle1 + "," + waterInBottle2 + ")");
+                }
+                // Check if any bottle contain the right amount of water
+                if (waterInBottle1 == soughtLitres || waterInBottle2 == soughtLitres) { break; }
+                else
+                {
+                    // If the sum of the water in both bottles contain more than
+                    // what second bottle can contain, only pour the amount it can hold
+                    // and remove that amount from the first bottle
+                    if (waterInBottle1 + waterInBottle2 > destinationBottle)
+                    {
+                        waterInBottle1 = waterInBottle1 - (destinationBottle - waterInBottle2);
+                        waterInBottle2 = destinationBottle;
+                        moves++;
+                        Console.WriteLine("Steg " + moves + " (" + waterInBottle1 + "," + waterInBottle2 + ")");
+                    }
+                    // Else empty the water in the first bottle into the second bottle
+                    else
+                    {
+                        waterInBottle2 = waterInBottle1 + waterInBottle2;
+                        waterInBottle1 = 0;
+                        moves++;
+                        Console.WriteLine("Steg " + moves + " (" + waterInBottle1 + "," + waterInBottle2 + ")");
+                    }
+                    // Check if any bottle contain the right amount of water
+                    if (waterInBottle1 == soughtLitres || waterInBottle2 == soughtLitres) { break; }
+                    else
+                    {
+                        // Empty second bottle if first bottle is not empty
+                        // Otherwise the loop will go back to start and we pour
+                        // the water from first bottle to second bottle
+                        if (waterInBottle1 != 0)
+                        {
+                            waterInBottle2 = 0;
+                            moves++;
+                            Console.WriteLine("Steg " + moves + " (" + waterInBottle1 + "," + waterInBottle2 + ")");
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("----------");
+            return moves;
+        }
+
+        public static int ShortestMoves(int rightMoves, int leftMoves)
+        {
+            var moves = new List<int>() { rightMoves, leftMoves };
+            return moves.Min();
+        }
+    }
+}
